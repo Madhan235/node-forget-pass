602e73c1fa418be84390c267e3c1a7ead82d0bfe
Madhan235 <msouljar@gmail.com>
2023-06-27 14:11:44 +0530

    cors-i

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { userRouter } from "./Routers/user.js";
 import dotenv from "dotenv";
 import { contentRouter } from "./Routers/content.js";
 import {isAuthenticated} from "./Authentication/auth.js";
-
+import cors from "cors"
 dotenv.config();
 // initializing server 
 const app = express();
 
 //middleware;
 app.use(express.json());
-
+app.use(cors());
  app.use("/users", userRouter)
  app.use("/movies",isAuthenticated, contentRouter)
 
